fd45088ca32b5219feab742097eceeff030ba14c
unknown <LAFRICAINE@LTP-DSI-SGLID.aaviebj.local>
2018-07-19 05:27:17 +0100

    first commit

--- components/thread_settings.js
+++ components/thread_settings.js
@@ -5,7 +5,7 @@ var debug = require('debug')('botkit:thread_settings');
 module.exports = function(controller) {
 
     debug('Configuring Facebook thread settings...');
-    controller.api.thread_settings.greeting('Hello! I\'m a Translate It bot!');
+    controller.api.thread_settings.greeting('Hello! I\'m a Rapid Trad bot! I can translate any sentences in French and English');
     controller.api.thread_settings.get_started('sample_get_started_payload');
     controller.api.thread_settings.menu([
         {
--- responses/responses.js
+++ responses/responses.js
@@ -2,23 +2,22 @@ module.exports = {
 	whichLanguage: {
 		"text" : "In which language would you like to translate this sentence ?",
 		"quick_replies":[
-	      {
-	        "content_type":"text",
-	        "title":"French",
-	        "payload":"fr"
-	      },
-	      {
-	        "content_type":"text",
-	        "title":"Spanish",
-	        "payload":"es"
-	      },
-	      {
+			{
+				"content_type":"text",
+				"title":"English",
+				"payload":"en"
+			},
+			{
+				"content_type":"text",
+				"title":"French",
+				"payload":"fr"
+			}
+	     /* {
 	        "content_type":"text",
 	        "title":"Dutch",
 	        "payload":"nl"
-	      }
+	      }*/
 	    ]
 	}, 
-	helloMessage: "Hey. I'm Translate It.\nI can help you translate sentences to your favourite langs ^_^.\nDon't worry about spelling errors ;)",
-	help: "Please send something similar to:\n\"translate i want to speak French\""
-}
+	helloMessage: "Hello! I\'m a RapidTraduction bot!\nI can help you translate sentences to English or French ^_^.\nDon't worry about spelling errors ;)\n For help type 'help' ",
+	help: "Please send something similar to:\n\"translate i want to speak French or traduit je veux parler Français\"",}
