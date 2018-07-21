8e9264d316c97cdd22c6b5a7f87014b6c220ccf9
unknown <LAFRICAINE@LTP-DSI-SGLID.aaviebj.local>
2018-07-21 11:06:41 +0100

    Made Changes In translation.js

--- skills/translation.js
+++ skills/translation.js
@@ -28,30 +28,7 @@ module.exports = function(controller) {
 
     });
 
-    controller.hears(['translate (.*)'], 'message_received', function(bot, message) {
-        bot.startTyping(message);
     
-        let sentence_to_translate = message.match[1];
-
-        bot.startConversation(message, function(err, convo) {
-
-            convo.ask(responses.whichLanguage, function(response, convo) {
-                
-                if(response.quick_reply){
-                    translate.translate(sentence_to_translate, response.quick_reply.payload, function(err, translatedLanguage) {
-                        
-                        if(!err){
-                            bot.reply(message, translatedLanguage);
-                        } else {
-                            bot.reply(message, "Please ask me again :)");
-                        }
-                    });
-                }
-                convo.next();
-            });
-        });
-
-    });
     
     controller.hears(['traduit (.*)'], 'message_received', function(bot, message) {
         bot.startTyping(message);
